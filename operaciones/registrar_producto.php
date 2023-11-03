745d81c889c9a4a2440030c8473add9796c5ca25
EAaronscm9702 <131274762+EAaronscm9702@users.noreply.github.com>
2023-11-03 09:47:49 -0500

    carrito-de-compra

---
+++ operaciones/registrar_producto.php
@@ -0,0 +1,12 @@
+<?php
+include("../include/conexion.php");
+
+$codi = $_POST['codi'];
+$descr = $_POST['descr'];
+$detall = $_POST['detall'];
+$precom = $_POST['precom'];
+$preven = $_POST['preven'];
+$stock = $_POST['stock'];
+$estad = $_POST['estad'];
+$imag = $_POST['imag'];
+?>
