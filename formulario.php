cc1ae14bec804f1a3abc01f6e15a28344c84ee3d
EAaronscm9702 <131274762+EAaronscm9702@users.noreply.github.com>
2023-11-10 12:32:23 -0500

    carrito-de-compra

--- formulario.php
+++ formulario.php
@@ -11,6 +11,12 @@ include("include/conexion.php");
     <link rel="stylesheet" href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" type="text/css"/>
     <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Plugins css -->
+    <link href="plantilla/Admin/vertical/" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical//plugins/datatables/responsive.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical//plugins/datatables/buttons.bootstrap4.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical//plugins/datatables/select.bootstrap4.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <?php 
@@ -24,75 +30,27 @@ include("include/conexion.php");
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-                        <h4>REGISTRO DE USUARIOS</h4>
+                    <?php include("include/modal_frm_reg_usuario.php"); ?>
+                        <h4>RELACIÓN DE USUARIOS</h4>
                         <div class="card">
                             <div class="card-body">
-                                <form action="operaciones/registrar_usuario.php" method="post" enctype="multipart/form-data">
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-md-3 col-sm-12">DNI:</label>
-                                        <input type="number"
-                                        name="dni"
-                                        placeholder=""
-                                        class="form-control col-lg-3 col-md-3 col-sm-12" required>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-md-3 col-sm-12">Nombres y Apellidos:</label>
-                                        <input type="text"
-                                        name="ApeyNom"
-                                        placeholder=""
-                                        class="form-control col-lg-9 col-md-9 col-sm-12" required>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-md-3 col-sm-12">Correo Electronico:</label>
-                                        <input type="email"
-                                        name="correo"
-                                        placeholder=""
-                                        class="form-control col-lg-9 col-md-9 col-sm-12" required>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-md-3 col-sm-12">Telefono:</label>
-                                        <input type="number"
-                                        name="telefono"
-                                        placeholder=""
-                                        class="form-control col-lg-3 col-md-3 col-sm-12" required>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-md-3 col-sm-12">Dirección:</label>
-                                        <input type="text"
-                                        name="direccion"
-                                        placeholder=""
-                                        class="form-control col-lg-9 col-md-9 col-sm-12" required>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-md-3 col-sm-12">Fecha de Nacimiento:</label>
-                                        <input type="date"
-                                        name="fechaNa"
-                                        placeholder=""
-                                        class="form-control col-lg-3 col-md-3 col-sm-12" required>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-md-3 col-sm-12">Rol:</label>
-                                        <select name="id_rol" id="">
-                                            <option value=""></option>
-                                            <?php
-                                            $b_roles = "SELECT * FROM roles";
-                                            $r_b_roles = mysqli_query($conexion, $b_roles);
-                                            while ($datos_roles = mysqli_fetch_array($r_b_roles)) { ?>
-                                                <option value="<?php echo $datos_roles['id'];?>"><?php echo $datos_roles['nombre']?></option>
-                                            <?php }
-                                            ?>
-                                            <option value="3">Cliente</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-md-3 col-sm-12">Foto:</label>
-                                        <input type="file" name="foto" placeholder="" class="form-control col-lg-3 col-md-3 col-sm-12" required accept="image/*">
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-md-3 col-sm-12"></label>
-                                        <button type="submit" class="btn btn-success">Guardar</button>
-                                    </div>
-                                </form>
+                                <table id="basic-datatable" class="table dt-responsive nowrap">
+                                    <thead>
+                                        <tr >
+                                            <th>Nro</th>
+                                            <th>Foto</th>
+                                            <th>DNI</th>
+                                            <th>Nombres y Apellidos</th>
+                                            <th>Correo</th>
+                                            <th>Teléfono</th>
+                                            <th>Dirección</th>
+                                            <th>Estado</th>
+                                            <th>Fecha de Nacimiento</th>
+                                            <th>Rol</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -108,6 +66,25 @@ include("include/conexion.php");
     <script src="plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
+
+    <!-- third party js -->
+    <script src="plantilla/Admin/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.bootstrap4.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.responsive.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/responsive.bootstrap4.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.buttons.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.bootstrap4.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.html5.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.flash.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/buttons.print.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.keyTable.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/dataTables.select.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/pdfmake.min.js"></script>
+    <script src="plantilla/Admin/plugins/datatables/vfs_fonts.js"></script>
+    <!-- third party js ends -->
+
+    <!-- Datatables init -->
+    <script src="plantilla/Admin/vertical/assets/pages/datatables-demo.js"></script>
 
     <!-- App js -->
     <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
